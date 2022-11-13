1e343f2cb33b3de0a5dee7f21edb8584a24d8c59
fakchich <mohammed.fakchich@gmail.com>
2022-11-13 08:21:12 +0100

    Added the assignement rules

--- angular-assi-practicing-directives/src/app/app.component.ts
+++ angular-assi-practicing-directives/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-assi-practicing-directives';
+  // <-- Add button wich says Display details
+
+  //
+
+  // <-- Add a paragraph to display the details
+
+  //
+
+  // <-- Toggle the displaying of the paragraph with the button
+
+  //
+
+  // <-- Log all the button clicks, and output them bellow the paragraph
+
+  //
+
+  // <-- Staring at the 5 log item, give all the log items a blue background, and white color using (ngClass & ngStyle)
+  
+  //
 }
